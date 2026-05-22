085ef41cf78a654b207ff3af10c4b1e00c06a07a
NuriyahD <nuriyahdavids2006@gmail.com>
2026-05-22 11:23:53 +0200

    Change console logs to show lengths of arrays
    Updated console logs to display array lengths instead of contents.

--- Exercise 3/arrays.js
+++ Exercise 3/arrays.js
@@ -3,5 +3,5 @@ colors.push('Red', 'Green', 'Blue');
 let numbers = [1, 2, 3, 4, 5];
 colors.pop(2);
 colors.unshift("Purple")
-console.log(colors)
-console.log(numbers)
+console.log(colors.length)
+console.log(numbers.length)
